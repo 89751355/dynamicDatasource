b9f1f971263b39d6720735865b84eb8c2493d083
siwangli <withasi@gmail.com>
2018-08-17 20:52:59 +0800

    axios

---
+++ template/src/request/api-type.js
@@ -0,0 +1,10 @@
+/* ************** 变量 ***************/
+export const BASE_URL = 'http://longcity.longhu.net:9090'// base url
+
+/* ************** service1 ***************/
+export const LOGIN = BASE_URL + '/login'// 登录
+export const SEARCH = BASE_URL + '/search'// 查询
+
+/* ************** service2 ***************/
+export const ADD = BASE_URL + '/add'// 添加
+export const DEL = BASE_URL + '/del'// 删除
